066baf4b7b6163cc5ae1f177921900ae4740501e
Francois Wouts <f@zenc.io>
2017-12-13 21:19:21 +1100

    Delete AWS loader, which relies on now-unused tags

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import "source-map-support/register";
 import * as awsAuth from "./service/aws/auth";
 import * as awsCluster from "./service/aws/cluster";
 import * as awsDeployment from "./service/aws/deployment";
-import * as awsLoader from "./service/aws/loader";
 import * as deployModel from "./service/deploymodel";
 import * as localLauncher from "./service/local/launcher";
 import * as path from "path";
@@ -34,8 +33,6 @@ const deploymentSpec: deployModel.DeploymentSpec = {
 
 async function main() {
   await awsAuth.authenticate();
-  console.log(await awsLoader.loadClusters());
-  console.log(await awsLoader.loadDeployments());
   await awsCluster.createCluster({
     name: "test",
     region: "ap-southeast-2",
