a8c32dd0cb419afb811907165ad4d144e45f11eb
Francois Wouts <f@zenc.io>
2017-12-13 17:38:44 +1100

    Set up Webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/main.ts",
+  devtool: "inline-source-map",
+  target: "node",
+  node: {
+    __dirname: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        use: ["remove-hashbag-loader"]
+      }
+    ]
+  },
+  resolveLoader: {
+    alias: {
+      "remove-hashbag-loader": path.join(
+        __dirname,
+        "./loaders/remove-hashbag-loader"
+      )
+    }
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
