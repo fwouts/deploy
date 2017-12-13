34bb2b5f07adf4ac2fe5cc760325e001f62740cc
Francois Wouts <f@zenc.io>
2017-12-13 18:15:31 +1100

    Add source map support
    This improves stack traces on crashes.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import "source-map-support/register";
+
 import * as awsAuth from "./service/aws/auth";
 import * as awsCluster from "./service/aws/cluster";
 import * as awsDeployment from "./service/aws/deployment";
