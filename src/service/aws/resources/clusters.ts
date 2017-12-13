0862d5b87db503b7b2a7f8166a014f51ac830f7c
Francois Wouts <f@zenc.io>
2017-12-13 21:19:47 +1100

    Delete unused code used for creation/deletion of individual resources (since we now use CloudFormation)

--- src/service/aws/resources/clusters.ts
+++ src/service/aws/resources/clusters.ts
@@ -1,5 +1,4 @@
 import * as ECS from "aws-sdk/clients/ecs";
-import * as loader from "../loader";
 
 export interface Cluster {
   name: string;
@@ -30,86 +29,4 @@ export async function getCluster(
   } else {
     throw new Error("Could not find existing cluster: " + clusterNameOrArn);
   }
-}
-
-export async function createCluster(
-  region: string,
-  name: string
-): Promise<string> {
-  // TODO: Double check the name's format.
-  let ecs = new ECS({
-    region: region
-  });
-  let existingClusterDescription = await ecs
-    .describeClusters({
-      clusters: [name]
-    })
-    .promise();
-  for (let existingCluster of existingClusterDescription.clusters || []) {
-    if (existingCluster.status === "ACTIVE") {
-      throw new Error(`A cluster with the name ${name} already exists.`);
-    }
-  }
-  let clusterCreation = await ecs
-    .createCluster({
-      clusterName: name
-    })
-    .promise();
-  if (!clusterCreation.cluster) {
-    throw new Error("Cluster could not be created.");
-  }
-  if (
-    !clusterCreation.cluster.clusterName ||
-    !clusterCreation.cluster.clusterArn
-  ) {
-    throw new Error("Cluster is missing key properties.");
-  }
-  return clusterCreation.cluster.clusterArn;
-}
-
-export async function destroyCluster(region: string, clusterArn: string) {
-  let ecs = new ECS({
-    region
-  });
-  let clusterDescription = await ecs
-    .describeClusters({
-      clusters: [clusterArn]
-    })
-    .promise();
-  if (
-    !clusterDescription.clusters ||
-    clusterDescription.clusters.length === 0
-  ) {
-    throw new Error("Cluster could not be found.");
-  }
-  let cluster = clusterDescription.clusters[0];
-  if (!cluster.clusterName) {
-    throw new Error("Cluster is missing a name.");
-  }
-  let containerInstanceArns = await loader.loadUntilEnd(async token => {
-    let { containerInstanceArns, nextToken } = await ecs
-      .listContainerInstances({
-        cluster: clusterArn,
-        nextToken: token
-      })
-      .promise();
-    return {
-      results: containerInstanceArns,
-      nextToken
-    };
-  });
-  for (let containerInstanceArn of containerInstanceArns) {
-    await ecs
-      .deregisterContainerInstance({
-        cluster: clusterArn,
-        containerInstance: containerInstanceArn,
-        force: true
-      })
-      .promise();
-  }
-  await ecs
-    .deleteCluster({
-      cluster: clusterArn
-    })
-    .promise();
 }
