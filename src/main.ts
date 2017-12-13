8a123ee51de166379a4366dfeb41462f88abb540
Francois Wouts <f@zenc.io>
2017-12-13 17:07:35 +1100

    Fix local launcher

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,33 @@ import * as awsAuth from "./service/aws/auth";
 import * as awsCluster from "./service/aws/cluster";
 import * as awsDeployment from "./service/aws/deployment";
 import * as awsLoader from "./service/aws/loader";
+import * as deployModel from "./service/deploymodel";
+import * as localLauncher from "./service/local/launcher";
 import * as path from "path";
+
+const deploymentSpec: deployModel.DeploymentSpec = {
+  name: "deployment",
+  cluster: {
+    region: "ap-southeast-2",
+    name: "test"
+  },
+  container: {
+    imageSource: {
+      type: "local",
+      dockerfilePath: path.resolve(
+        __dirname,
+        "..",
+        "example-server",
+        "Dockerfile"
+      )
+    },
+    memory: 512
+  },
+  desiredCount: 1,
+  environment: {
+    TEST: "Hello, World!"
+  }
+};
 
 async function main() {
   await awsAuth.authenticate();
@@ -16,30 +42,10 @@ async function main() {
       ec2InstanceType: "t2.micro"
     });
   } catch {}
-  await awsDeployment.deploy(
-    {
-      name: "deployment",
-      cluster: {
-        region: "ap-southeast-2",
-        name: "test"
-      },
-      container: {
-        imageSource: {
-          type: "local",
-          dockerfilePath: path.resolve(
-            __dirname,
-            "..",
-            "example-server",
-            "Dockerfile"
-          )
-        },
-        memory: 512
-      },
-      desiredCount: 1,
-      environment: {}
-    },
-    "mydeployment"
-  );
+  try {
+    await awsDeployment.deploy(deploymentSpec, "mydeployment");
+  } catch {}
+  await localLauncher.deploy(deploymentSpec);
 }
 
 main().catch(console.error);
