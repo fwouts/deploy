3131cee9afd4e223add22c806ed2ccb2154dff0a
Francois Wouts <f@zenc.io>
2017-12-14 18:00:01 +1100

    Improve error message when no AWS credentials can be found
    Fixes #1.

--- src/service/aws/auth.ts
+++ src/service/aws/auth.ts
@@ -1,4 +1,5 @@
 import * as AWS from "aws-sdk/global";
+import * as console from "../console";
 
 export async function authenticate(): Promise<void> {
   try {
@@ -6,7 +7,10 @@ export async function authenticate(): Promise<void> {
     AWS.config.credentials = credentials;
     await credentials.getPromise();
   } catch (e) {
-    console.error(e);
+    if (e.code === "ENOENT") {
+      console.logError("No AWS credentials found. Please run `aws configure`.");
+      throw new console.AlreadyLoggedError(e);
+    }
     throw e;
   }
 }
