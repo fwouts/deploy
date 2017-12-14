005125c7a3182673f5d0e792f738931cb31bea57
Francois Wouts <f@zenc.io>
2017-12-14 13:01:33 +1100

    Add explicit commands to list, create and delete clusters

--- src/main.ts
+++ src/main.ts
@@ -2,57 +2,86 @@ import "source-map-support/register";
 
 import * as awsAuth from "./service/aws/auth";
 import * as awsCluster from "./service/aws/cluster/adhoc";
-import * as awsDeployment from "./service/aws/deployment/adhoc";
 import * as awsLoader from "./service/aws/loader";
-import * as deployModel from "./service/deploymodel";
-import * as localLauncher from "./service/local/launcher";
-import * as path from "path";
+import * as console from "./service/console";
+import * as program from "commander";
 
-const deploymentSpec: deployModel.DeploymentSpec = {
-  name: "deployment",
-  cluster: {
-    region: "ap-southeast-2",
-    name: "test"
-  },
-  container: {
-    imageSource: {
-      type: "local",
-      dockerfilePath: path.resolve(
-        __dirname,
-        "..",
-        "example-server",
-        "Dockerfile"
-      )
-    },
-    memory: 512
-  },
-  desiredCount: 1,
-  environment: {
-    TEST: "Hello, World!"
-  }
-};
+// TODO: Make sure this stays in sync with package.json.
+const VERSION = "0.0.4";
 
-async function main() {
-  await awsAuth.authenticate();
-  console.log(await awsLoader.loadClusters());
-  console.log(await awsLoader.loadDeployments());
-  await awsCluster.createCluster({
-    name: "test",
-    region: "ap-southeast-2",
-    ec2InstanceCount: 1,
-    ec2InstanceType: "t2.micro"
-  });
-  await awsDeployment.deploy(deploymentSpec, "mydeployment");
-  await awsDeployment.destroy(
-    deploymentSpec.cluster.region,
-    deploymentSpec.cluster.name,
-    "mydeployment"
-  );
-  await awsCluster.destroy(
-    deploymentSpec.cluster.region,
-    deploymentSpec.cluster.name
-  );
-  await localLauncher.deploy(deploymentSpec);
+program.version(VERSION);
+
+function asyncAction(f: (...args: any[]) => Promise<any>) {
+  return (...args: any[]) => {
+    f(...args).catch(error => {
+      console.logError(error);
+    });
+  };
 }
 
-main().catch(console.error);
+program.command("list-clusters").action(
+  asyncAction(async () => {
+    await awsAuth.authenticate();
+    let clusters = await awsLoader.loadClusters();
+    console.logInfo(JSON.stringify(clusters, null, 2));
+  })
+);
+
+program
+  .command("create-cluster <name>")
+  .option(
+    "-r, --region <region>",
+    'The region in which to set up the cluster. Example: "us-east-1".',
+    "us-east-1"
+  )
+  .option(
+    "-t, --instance_type <instance-type>",
+    'The type of instance to start. Example: "t2.micro".',
+    "t2.micro"
+  )
+  .option(
+    "-c, --instance_count <instance-count>",
+    "The number of instances to start. Example: 1.",
+    parseInt,
+    1
+  )
+  .action(
+    asyncAction(
+      async (
+        name: string,
+        options: {
+          region: string;
+          instance_type: string;
+          instance_count: number;
+        }
+      ) => {
+        await awsAuth.authenticate();
+        await awsCluster.createCluster({
+          name: name,
+          region: options.region,
+          ec2InstanceType: options.instance_type,
+          ec2InstanceCount: options.instance_count
+        });
+      }
+    )
+  );
+
+program
+  .command("destroy-cluster <name>")
+  .option(
+    "-r, --region <region>",
+    'The region in which the cluster was set up. Example: "us-east-1".',
+    "us-east-1"
+  )
+  .action(
+    asyncAction(async (name: string, options: { region: string }) => {
+      await awsAuth.authenticate();
+      await awsCluster.destroy(options.region, name);
+    })
+  );
+
+program.command("*").action(cmd => {
+  console.logError(`Unknown command: ${cmd}.`);
+});
+
+program.parse(process.argv);
--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
     })
   ],
   output: {
-    filename: "bundle.js",
+    filename: "main.js",
     path: path.resolve(__dirname, "dist")
   }
 };
