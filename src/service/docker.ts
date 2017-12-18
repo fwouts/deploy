6eb0c9b150038fe049702050e2c30737dae92824
Francois Wouts <f@zenc.io>
2017-12-18 14:29:34 +1100

    Remove unneeded import

--- src/service/docker.ts
+++ src/service/docker.ts
@@ -1,7 +1,6 @@
 import * as Docker from "dockerode";
 import * as console from "./console";
 import * as fs from "fs";
-import * as os from "os";
 import * as path from "path";
 import * as tar from "tar-fs";
 
