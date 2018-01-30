0a097b3bd85dd2d7e847d682f51b2cbe9a12ba2c
Francois Wouts <f@zenc.io>
2018-01-30 11:35:05 +1100

    Release 0.0.11

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ import * as program from "commander";
 import * as updateCheck from "./update-check";
 
 // TODO: Make sure this stays in sync with package.json.
-export const VERSION = "0.0.10";
+export const VERSION = "0.0.11";
 
 program.version(VERSION);
 
